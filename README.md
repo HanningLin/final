b771fcdbf04aec8189f4c55c85d394889c582840
HanningLin <liam.hanninglin@outlook.com>
2019-05-16 00:58:03 +0800

    done

---
+++ README.md
@@ -0,0 +1,60 @@
+# Final Project 直线检测
+--------------------
+自动化52林汉宁
+2150504042
+
+
+## 作业要求
+------------------------
+1. 首先对测试图像（文件名为：test1~test6）进行边缘检测，可采用书上介绍的Sobel等模板或者cann算子方法；
+2. 在边缘检测的基础上，用hough变换检测图中直线；
+3. 比较不同边缘检测算法（2种以上）、不同hough变换参数对直线检测的影响；
+4. 可以采用Matlab、OpenCV等自带函数。
+## 摘要
+-------------------------
+本次实验基于Python+Opencv实现对图像的碧娜院检测与直线检测。分别使用Canny与Sobel木模板进行边缘检测，之后使用Hough变换检测图中直线。
+## 实验内容
+-----------------------
+### 1.算法分析
+- 边缘检测可使用canny sobel等算法，每种算法都有自己的算子，使用相应算子进行滤波即可。
+再opencv 中有相应借口可方便实现。具体函数为
+` cv2.Sobel`与`cv2.Canny`
+- 在完成边缘检测后，使用hough变换检测图中直线。霍夫变换(Hough Transform)是图像处理中的一种特征提取技术，它通过一种投票算法检测具有特定形状的物体。该过程在一个参数空间中通过计算累计结果的局部最大值得到一个符合该特定形状的集合作为霍夫变换结果。在Opencv中可使用`cv2.HoughLinesP`函数实现hough变换直线检测。
+### 2.结果展示与分析
+-------------------------
+#### test1：
+![enter image description here](capturepic/test1_1.png)
+![enter image description here](capturepic/test1_2.png)
+- 对Canny 的霍夫变换minLineLength为10，maxLineGap为20
+- 对Sobel处理后的霍夫变换minLineLength为200，maxLineGap为1
+#### test2:
+![enter image description here](capturepic/test2_1.png)
+![enter image description here](capturepic/test2_2.png)
+- 对Canny 的霍夫变换minLineLength为20，maxLineGap为30
+- 对Sobel处理后的霍夫变换minLineLength为200，maxLineGap为1
+ #### test3:
+![enter image description here](capturepic/test3_1.png)
+![enter image description here](capturepic/test3_2.png)
+- 对Canny 的霍夫变换minLineLength为30，maxLineGap为30
+- 对Sobel处理后的霍夫变换minLineLength为160，maxLineGap为1
+ #### test4:
+![enter image description here](capturepic/test4_1.png)
+![enter image description here](capturepic/test4_2.png)
+- 对Canny 的霍夫变换minLineLength为120，maxLineGap为30
+- 对Sobel处理后的霍夫变换minLineLength为120，maxLineGap为1
+ #### test5:
+![enter image description here](capturepic/test5_1.png)
+![enter image description here](capturepic/test5_2.png)
+- 对Canny 的霍夫变换minLineLength为10，maxLineGap为3
+- 对Sobel处理后的霍夫变换minLineLength为40，maxLineGap为1
+ #### test6:
+![enter image description here](capturepic/test6_1.png)
+![enter image description here](capturepic/test6_2.png)
+- 对Canny 的霍夫变换minLineLength为100，maxLineGap为3
+- 对Sobel处理后的霍夫变换minLineLength为400，maxLineGap为1
+
+
+#### 结论分析：
+- Sobel法与Canny法对比，Canny得到的结果更好，Sobel留下细节过多。Canny法结果更适于继续进行直线检测。
+- 针对Sobel法后Hough变换，由于细节过多，所以调参时minLineLength更大，maxLineGap更小。
+- 当minLineLength更大，maxLineGap更小时得到直线结果变少。反之亦然。此结论可用于调参得到更好结果。
--- final.py
+++ final.py
@@ -1,27 +1,68 @@
 import numpy
 import argparse
 import cv2
-def sober(path):
-    image = cv2.imread(path，newpath)
+def sober(path,newpath):
+    image = cv2.imread(path)
     #cv2.imshow("Original", image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #cv2.imshow("Gray", gray)
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
-    sobelx = numpy.uint8(numpy.absolute(sobelx))
-    sobely = numpy.uint8(numpy.absolute(sobely))
-    sobelcombine = cv2.bitwise_or(sobelx,sobely)
+    scale = 1
+    delta = 0
+    ddepth = cv2.CV_16S
+    sobelx=cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    sobely=cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    sobelx=cv2.convertScaleAbs(sobelx,sobelx)
+    sobely=cv2.convertScaleAbs(sobely,sobely)
+    grad = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
     #display two images in a figure
         # cv2.imshow("Edge detection by Sobel", numpy.hstack([gray,sobelx,sobely, sobelcombine]))
-    cv2.imwrite(newpath, numpy.hstack([gray,sobelx,sobely, sobelcombine]))
+    #cv2.imwrite(newpath, numpy.hstack([gray,sobelx,sobely, sobelcombine]))
+    cv2.imwrite(newpath,grad)
 
-    # f(cv2.waitKey(0)==27):
-    #     cv2.destroyAllWindows()
 
-sober（'test1.tif'，'result/test1_edge_by_sobel.jpg'）
-sober（'test2.png'，'result/test2_edge_by_sobel.jpg'）
-sober（'test3.jpg'，'result/test3_edge_by_sobel.jpg'）
-sober（'test4.bmp'，'result/test4_edge_by_sobel.jpg'）
-sober（'test5.png'，'result/test5_edge_by_sobel.jpg'）
-sober（'test6.jpg'，'result/test6_edge_by_sobel.jpg'）
+def hough(path,newpath1,newpath2,Length,Gap,Length1,Gap1,num):
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_back=img
+    img1 = cv2.Canny(img, 50, 200, None, 3)
+    cv2.imwrite('canny'+str(num)+'.jpg',img1)
+    lines1 = cv2.HoughLinesP(img1, 1, numpy.pi / 180, 50, None, Length, Gap)
+    #lines = cv2.HoughLinesP(img,1.0,numpy.pi/180,20,None,minLineLength,maxLineGap)
+    lines1 = lines1[:,0,:]
+    for x1,y1,x2,y2 in lines1[:]:
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,0),3)
+    cv2.imwrite(newpath2,img)
+
+
+    scale = 1
+    delta = 0
+    ddepth = cv2.CV_16S
+    sobelx=cv2.Sobel(img, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    sobely=cv2.Sobel(img, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    sobelx=cv2.convertScaleAbs(sobelx,sobelx)
+    sobely=cv2.convertScaleAbs(sobely,sobely)
+    grad = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
+    lines2 = cv2.HoughLinesP(grad, 1, numpy.pi / 180, 50, None, Length1, Gap1)
+    #lines = cv2.HoughLinesP(img,1.0,numpy.pi/180,20,None,minLineLength,maxLineGap)
+    lines2 = lines2[:,0,:]
+    for x1,y1,x2,y2 in lines2[:]:
+        cv2.line(img_back,(x1,y1),(x2,y2),(255,0,0),2)
+    cv2.imwrite(newpath1,img_back)
+
+
+sober('test1.tif','result/test1_edge_by_sobel.jpg')
+sober('test2.png','result/test2_edge_by_sobel.jpg')
+sober('test3.jpg','result/test3_edge_by_sobel.jpg')
+sober('test4.bmp','result/test4_edge_by_sobel.jpg')
+sober('test5.png','result/test5_edge_by_sobel.jpg')
+sober('test6.jpg','result/test6_edge_by_sobel.jpg')
     
+hough('test1.tif','result/test1_sobel_hough.jpg','result/test1_canny_hough.jpg',10,20,200,1,1)
+hough('test2.png','result/test2_sobel_hough.jpg','result/test2_canny_hough.jpg',20,30,200,1,2)
+hough('test3.jpg','result/test3_sobel_hough.jpg','result/test3_canny_hough.jpg',30,30,160,1,3)
+hough('test4.bmp','result/test4_sobel_hough.jpg','result/test4_canny_hough.jpg',120,30,120,1,4)
+hough('test5.png','result/test5_sobel_hough.jpg','result/test5_canny_hough.jpg',10,3,40,1,5)
+hough('test6.jpg','result/test6_sobel_hough.jpg','result/test6_canny_hough.jpg',100,3,400,1,6)
+
+
+
